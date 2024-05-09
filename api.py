00793ed7bd62f63ab18aea3f9e024cd94e2950f1
Muneeb502 <mu0476080@gmail.com>
2024-05-09 13:31:05 +0500

    adding some code

--- api.py
+++ api.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-     return "hello"
+     return render_template("index.html")
  
+@app.route("/adddata")
+def adddata():
+    return render_template("form.html")
+     
+@app.route("/submitform" method= ["get"])
+def submit():
+    if request.method == "post":
+        
+        
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
